18f6bb78c1b01ead503ea487d8ee4ec1724a4c89
Vivian Phung <9276753+VPhung24@users.noreply.github.com>
2023-06-16 20:39:42 -0400

    error handling

--- snowballwebapp/settings.py
+++ snowballwebapp/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ["SECRET_KEY"]
 
-DEBUG = False
+DEBUG = int(os.environ.get("DEBUG", 1))
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
 

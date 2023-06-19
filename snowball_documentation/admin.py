7073a58d706a0e50bb006eac069e80ff73ac23b0
Vivian Phung <9276753+VPhung24@users.noreply.github.com>
2023-06-19 17:37:50 -0400

    docs via gitbooks

--- snowball_documentation/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Documentation
-
-admin.site.register(Documentation)
--- snowballwebapp/urls.py
+++ snowballwebapp/urls.py
@@ -17,11 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 from snowball_main import views
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("snowball_main.urls")),
-    path("documentation/", include("snowball_documentation.urls")),
-]
+urlpatterns = [path("admin/", admin.site.urls), path("", include("snowball_main.urls"))]
 
 handler404 = views.page_not_found
 handler500 = views.server_error

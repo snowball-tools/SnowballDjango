d2f15ec8106641137474ab3f2283c15328cbcf1c
Vivian Phung <9276753+VPhung24@users.noreply.github.com>
2023-06-20 13:51:19 -0400

    working but unstyled

--- staticfiles/snowball_main/js/draggable.js
+++ staticfiles/snowball_main/js/draggable.js
@@ -8,8 +8,13 @@ function dragElement(elmnt) {
     pos2 = 0,
     pos3 = 0,
     pos4 = 0;
-
-  elmnt.onmousedown = dragMouseDown;
+  if (document.getElementById(elmnt.id + "header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
 
   function dragMouseDown(e) {
     e = e || window.event;

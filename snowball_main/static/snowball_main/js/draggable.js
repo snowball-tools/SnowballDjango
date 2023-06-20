b7e42800f3a1f0130bb652638b8e77b38a5460d1
Vivian Phung <9276753+VPhung24@users.noreply.github.com>
2023-06-20 13:10:41 -0400

    draggable start

---
+++ snowball_main/static/snowball_main/js/draggable.js
@@ -0,0 +1,33 @@
+var elements = document.getElementsByClassName("draggable-element");
+
+function drag_start(event) {
+  var style = window.getComputedStyle(event.target, null);
+  event.dataTransfer.setData(
+    "text/plain",
+    parseInt(style.getPropertyValue("left"), 10) -
+      event.clientX +
+      "," +
+      (parseInt(style.getPropertyValue("top"), 10) - event.clientY)
+  );
+}
+
+function drag_over(event) {
+  event.preventDefault();
+  return false;
+}
+
+function drop(event) {
+  var offset = event.dataTransfer.getData("text/plain").split(",");
+  var dm = document.getElementById("dragme");
+  dm.style.left = event.clientX + parseInt(offset[0], 10) + "px";
+  dm.style.top = event.clientY + parseInt(offset[1], 10) + "px";
+  event.preventDefault();
+  return false;
+}
+
+for (var i = 0; i < elements.length; i++) {
+  elements[i].addEventListener("dragstart", drag_start, false);
+}
+
+document.body.addEventListener("dragover", drag_over, false);
+document.body.addEventListener("drop", drop, false);

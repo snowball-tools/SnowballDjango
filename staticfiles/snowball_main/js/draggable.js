c8d4021417492c9738066529aa6a93ecb85b5b12
Vivian Phung <9276753+VPhung24@users.noreply.github.com>
2023-06-20 14:01:18 -0400

    styled boxes

--- staticfiles/snowball_main/js/draggable.js
+++ staticfiles/snowball_main/js/draggable.js
@@ -8,13 +8,8 @@ function dragElement(elmnt) {
     pos2 = 0,
     pos3 = 0,
     pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    // if present, the header is where you move the DIV from:
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
-    elmnt.onmousedown = dragMouseDown;
-  }
+
+  elmnt.onmousedown = dragMouseDown;
 
   function dragMouseDown(e) {
     e = e || window.event;

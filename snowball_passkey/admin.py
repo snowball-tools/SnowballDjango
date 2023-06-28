3902d7d2d7b49b3eca432c5805060043f8161e0b
Vivian Phung <9276753+VPhung24@users.noreply.github.com>
2023-06-27 20:33:15 -0400

    auth

--- snowball_passkey/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import SnowballAuth
-
-
-@admin.register(SnowballAuth)
-class SnowballAuthAdmin(admin.ModelAdmin):
-    list_display = ["user", "name", "created_on", "last_used_on"]
-    search_fields = ["name", "credential_id"]

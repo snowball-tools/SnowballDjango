c326ee2a080e48422172726a00a887316d394a7c
Vivian Phung <9276753+VPhung24@users.noreply.github.com>
2023-06-30 23:24:26 -0400

    auth adapter fix naming

--- snowballwebapp/settings.py
+++ snowballwebapp/settings.py
@@ -5,12 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ["SECRET_KEY"]
 
-DEBUG = int(os.environ.get("DEBUG", 1)) == 1
-
-if DEBUG:
-    os.environ["HTTPS"] = "on"
-    os.environ["SSL_CERT_FILE"] = os.path.join(BASE_DIR, "localhost.crt")
-    os.environ["SSL_KEY_FILE"] = os.path.join(BASE_DIR, "localhost.key")
+DEBUG = os.environ["DEBUG"] == "True"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
 

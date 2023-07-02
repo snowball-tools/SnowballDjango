957d238440455b9acf1275d85eb5b225b7ff946c
Vivian Phung <9276753+VPhung24@users.noreply.github.com>
2023-07-02 19:45:21 -0400

    debug

--- snowballwebapp/settings.py
+++ snowballwebapp/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ["SECRET_KEY"]
 
-DEBUG = True
+DEBUG = os.environ["DEBUG"] == "True"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
 

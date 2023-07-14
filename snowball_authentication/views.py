a5861a15bc2ab0d2bd247567fce5d569ecaac2fc
Vivian Phung <9276753+VPhung24@users.noreply.github.com>
2023-07-14 20:30:52 +0200

    lit

--- snowball_authentication/views.py
+++ snowball_authentication/views.py
@@ -1,10 +1,12 @@
+import os
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 
 @login_required(login_url="{% url 'login' %}")
 def profile(request):
-    return render(request, "profile.html")
+    relay_key = os.environ.get("RELAY_KEY")
+    return render(request, "profile.html", {"RELAY_KEY": relay_key})
 
 
 @login_required(login_url="{% url 'login' %}")
